a7423808c792b5be2b2cf486aec24abec0dba168
marstc1 <christopher.marston@aviva.co.uk>
2013-11-15 09:46:09 +0000

    First build

---
+++ VivaRevolution/Controllers/PosterController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VivaRevolution.Models;
+
+namespace VivaRevolution.Controllers
+{
+    public class PosterController : Controller
+    {
+        public ActionResult Index()
+        {
+            PosterViewModel vm = new PosterViewModel
+            {
+                ImgId = "tb",
+                Quote = "When I invented the web, I didnt have to ask anyones permission",
+                Name = "Sir Tim Berners-Lee",
+                Title = "Inventor of the world wide web",
+                TagLine = "Join the digital revolution"
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string imgId, string quote, string name, string title, string tagline)
+        {
+            return RedirectToAction("View", new { @ImgId = imgId, @Quote = quote, @Name = name, @Title = title, @TagLine = tagline });
+        }
+
+        public ActionResult View(string imgId, string quote, string name, string title, string tagline)
+        {
+            PosterViewModel vm = new PosterViewModel
+            {
+                ImgId = (!String.IsNullOrEmpty(imgId)) ? imgId : "tb",
+                Quote = (!String.IsNullOrEmpty(quote)) ? quote.ToUpper() : string.Empty,
+                Name = (!String.IsNullOrEmpty(name)) ? name.ToUpper(): string.Empty,
+                Title = (!String.IsNullOrEmpty(title)) ? string.Format(" - {0}", title) : string.Empty,
+                TagLine = (!String.IsNullOrEmpty(tagline)) ? tagline : "Join the digital revolution",
+            };
+            
+            return View("View", vm);
+        }
+
+        public ActionResult Vader()
+        {
+            string imgId = "dv";
+            string quote = "when I invented the death star, I didnt have to ask anyones permission";
+            string name = "darth vader";
+            string title = "sith lord, supreme commander";
+            string tagline = "Join the dark side";
+
+            return RedirectToAction("View", new { @ImgId = imgId, @Quote = quote, @Name = name, @Title = title, @TagLine = tagline });
+        }
+    }
+}
---
+++ VivaRevolution/Models/PosterViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VivaRevolution.Models
+{
+    public class PosterViewModel
+    {
+        private string imgId;
+        private string quote;
+        private string name;
+        private string title;
+        private string tagLine;
+
+        public string ImgId 
+        {
+            get
+            {
+                return this.imgId;
+            }
+            set
+            {
+                this.imgId = value;
+            }
+        }
+
+        public string Quote
+        {
+            get
+            {
+                return this.quote;
+            }
+            set
+            {
+                this.quote = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Title 
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                this.title = value;
+            }
+        }
+
+        public string TagLine 
+        {
+            get
+            {
+                return this.tagLine;
+            }
+            set
+            {
+                this.tagLine = value;
+            }
+        }
+    }
+}
