b98d7bb48128a6711e2a0d750555921b896e7bbe
marstc1 <christopher.marston@aviva.co.uk>
2013-11-15 16:59:56 +0000

    Updated styles and added poster to first page

--- VivaRevolution/Models/Mappers/PosterViewModelMapper.cs
+++ VivaRevolution/Models/Mappers/PosterViewModelMapper.cs
@@ -10,13 +10,16 @@ namespace VivaRevolution.Models.Mappers
         public List<PosterImage> CreatePosterImagesDropDownValues() {
             List<PosterImage> list = new List<PosterImage>();
 
+            list.Add(new PosterImage { ImageValue = "sj", ImageName = "Steve Jobs" });
+            list.Add(new PosterImage { ImageValue = "tb", ImageName = "Tim Berners-Lee" });
             list.Add(new PosterImage { ImageValue = "dv", ImageName = "Darth Vader" });
             list.Add(new PosterImage { ImageValue = "hs", ImageName = "Han Solo" });
             list.Add(new PosterImage { ImageValue = "r2", ImageName = "R2D2" });
-            list.Add(new PosterImage { ImageValue = "sj", ImageName = "Steve Jobs" });
             list.Add(new PosterImage { ImageValue = "st", ImageName = "Storm Trooper" });
-            list.Add(new PosterImage { ImageValue = "tb", ImageName = "Tim Berners-Lee" });
             list.Add(new PosterImage { ImageValue = "yd", ImageName = "Yoda" });
+            list.Add(new PosterImage { ImageValue = "ba", ImageName = "Mr T" });
+            list.Add(new PosterImage { ImageValue = "hsm", ImageName = "George Peppard" });
+            list.Add(new PosterImage { ImageValue = "bd", ImageName = "Bob Dylan" });
 
             return list;
         }
